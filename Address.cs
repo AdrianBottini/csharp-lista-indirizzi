f90e9123802e3773c7a96d4631507efc713c3850
adrianboh@gmail.com <adrianboh@gmail.com>
2023-03-25 10:31:10 +0100

    esercizio completo

---
+++ Address.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+public class Address
+{
+    public Address(string name, string surname, string street, string city, string province, string zIP)
+    {
+        Name = name;
+        Surname = surname;
+        Street = street;
+        City = city;
+        Province = province;
+        ZIP = zIP;
+    }
+    public Address(string? name, string? surname, string? secondName, string street, string city, string province, string zIP)
+    {
+        Name = name;
+        Surname = surname;
+        SecondName = secondName;
+        Street = street;
+        City = city;
+        Province = province;
+        ZIP = zIP;
+    }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string SecondName { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public string ZIP { get; set; }
+}
---
+++ Utility/Parser.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class Parser
+{
+    public const string InputFile = "..\\..\\..\\addresses.csv";
+    public const string OutputFile = "..\\..\\..\\addresses.txt";
+
+    public static IEnumerable<Address> Read()
+    {
+        using var input = File.OpenText(InputFile);
+        var addresses = new List<Address>();
+        input.ReadLine();
+        int counter = 1;
+        var ud = "(unknowData)";
+        while (!input.EndOfStream)
+        {
+            var line = input.ReadLine();
+            counter++;
+            var chunks = line.Split(",")!;
+            for (int i = 0; i < chunks.Length; i++)
+            {
+                if (chunks[i] == "") chunks[i] = ud;
+            }
+
+
+            if (chunks.Length == 7)
+            {
+                var name = chunks[0];
+                var surname = chunks[1];
+                var secondName = chunks[2];
+                var street = chunks[3];
+                var city = chunks[4];
+                var province = chunks[5];
+                var ZIP = chunks[6];
+                var address = new Address(name, surname, secondName, street, city, province, ZIP);
+                addresses.Add(address);
+            }
+            else if (chunks.Length == 4)
+            {
+                var name = chunks[0];
+                var surname = ud;
+                var street = ud;
+                var city = chunks[1];
+                var province = chunks[2];
+                var ZIP = chunks[3];
+                var address = new Address(name, surname, street, city, province, ZIP);
+                addresses.Add(address);
+
+            }
+            else if (chunks.Length == 6)
+            {
+                var name = chunks[0];
+                var surname = chunks[1];
+                var street = chunks[2];
+                var city = chunks[3];
+                var province = chunks[4];
+                var ZIP = chunks[5];
+                var address = new Address(name, surname, street, city, province, ZIP);
+                addresses.Add(address);
+            }
+            else
+            {
+                Console.WriteLine($"La riga n. {counter} non è valida");
+                continue;
+            }
+
+        }
+        return addresses;
+    }
+
+    public static void Write(IEnumerable<Address> addresses)
+    {
+        using var output = File.CreateText(OutputFile);
+        output.WriteLine("Lista di Indirizzi:");
+        foreach (var address in addresses)
+        {
+            output.WriteLine();
+            output.WriteLine("----------------");
+            output.WriteLine($"Nome e Cognome: {address.Name} {address.SecondName} {address.Surname}");
+            output.WriteLine($"Indirizzo: {address.Street}, {address.City} ({address.Province}), {address.ZIP}");
+            output.WriteLine("----------------");
+
+        }
+    }
+}
